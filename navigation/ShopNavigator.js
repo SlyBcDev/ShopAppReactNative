375490af8e6cb1e68c873f5709013c3b3cb7d6d1
SlyBcDev <slybcdev@free.fr>
2020-05-26 16:56:21 +0200

    productitem ok

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,24 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {Platform} from 'react-native';
+
+import ProductsOverviewScreen from '../screens/shop/ProductOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import Colors from '../constants/Colors';
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+    },
+  },
+);
+
+export default createAppContainer(ProductsNavigator);
