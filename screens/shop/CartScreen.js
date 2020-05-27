00a148a8bf76c0ab4e5dea9e6d0a9e0c82f8b980
SlyBcDev <slybcdev@free.fr>
2020-05-27 11:53:22 +0200

    add product on cart

---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {Text, View, FlatList, Button, StyleSheet} from 'react-native';
+import Colors from '../../constants/Colors';
+import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cart';
+
+const CartScreen = (props) => {
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const cartItems = useSelector((state) => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum,
+      });
+    }
+    return transformedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1,
+    );
+  });
+
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.summary}>
+        <Text style={styles.summaryText}>
+          Total:
+          <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+        </Text>
+        <Button
+          color={Colors.accent}
+          title="Order Now"
+          disabled={cartItems.length === 0}
+        />
+      </View>
+      <View>
+        <Text>Cart Items</Text>
+      </View>
+      <FlatList
+        data={cartItems}
+        keyExtractor={(items) => items.productId}
+        renderItem={(cartItems) => (
+          <CartItem
+            quantity={cartItems.item.quantity}
+            title={cartItems.item.productTitle}
+            amount={cartItems.item.sum}
+            onRemove={() => {
+              dispatch(cartActions.removeFromCart(cartItems.item.productId));
+            }}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20,
+  },
+  summary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+  summaryText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  amount: {
+    color: Colors.primary,
+  },
+});
+
+export default CartScreen;
